b4cfbe7ec57837e90e2d10feeac74211915ffcf2
viggy6 <vignesh6boppana@gmail.com>
2022-09-20 18:13:00 -0500

    #1 added endpoints for CRUD on products

--- src/main/java/com/cvkart/service/entity/Product.java
+++ src/main/java/com/cvkart/service/entity/Product.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -15,6 +17,7 @@ import javax.persistence.Id;
 public class Product {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer productId;
     private String productName;
     private String productBrand;
